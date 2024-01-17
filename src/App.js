7bf69b9293963b921d92d6db0d7aad34cd1f0096
arghya banerjee <arghyabanerjee59@gmail.com>
2024-01-17 20:01:46 +0530

    Breezy V2 App initial structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+//React
+import { React, createContext, useEffect, useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
+
+export default function App() {
+    return(
+        <>
+            Hello
+        </>
+    )
+}

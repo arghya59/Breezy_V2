0aaf7e577ef523ee7cd0b3bcaa0df558f1d4c648
Arghya Banerjee <arghyabanerjee@Arghyas-MacBook-Air.local>
2024-02-27 00:17:41 +0530

    Resolve minor issue

--- src/universal-components/Footer.jsx
+++ src/universal-components/Footer.jsx
@@ -4,9 +4,10 @@ import './style.css'
 export default function Footer() {
   const date = new Date()
   const year = date.getFullYear()
+  const name = process.env.REACT_PUBLIC_NAME;
   return (
     <div className='footer'>
-               <div>Copyright © {year} Breezy by Arghya Banerjee All Right Reserved. Powered by <a rel="noreferrer" target="_blank" href='https://openweathermap.org/'>OpenWeather</a></div>
+               <div>Copyright © {year} Breezy by {name} All Right Reserved. Powered by <a rel="noreferrer" target="_blank" href='https://openweathermap.org/'>OpenWeather</a></div>
     </div>
   )
 }

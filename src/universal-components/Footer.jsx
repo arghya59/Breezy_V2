565352bc5df88040cbf4a7590c6a52faa9c46383
Arghya Banerjee <arghyabanerjee@Arghyas-MacBook-Air.local>
2024-02-27 00:46:00 +0530

    test env

--- src/universal-components/Footer.jsx
+++ src/universal-components/Footer.jsx
@@ -4,7 +4,7 @@ import './style.css'
 export default function Footer() {
   const date = new Date()
   const year = date.getFullYear()
-  const name = process.env.REACT_PUBLIC_NAME;
+  const name = process.env.REACT_APP_NAME;
   return (
     <div className='footer'>
                <div>Copyright © {year} Breezy by {name} All Right Reserved. Powered by <a rel="noreferrer" target="_blank" href='https://openweathermap.org/'>OpenWeather</a></div>

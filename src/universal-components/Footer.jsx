6f7917b8a20871ece721c36a1ff8effcfa32ace8
Arghya Banerjee <arghyabanerjee@Arghyas-MacBook-Air.local>
2024-02-27 00:53:57 +0530

    text env 2

--- src/universal-components/Footer.jsx
+++ src/universal-components/Footer.jsx
@@ -5,9 +5,10 @@ export default function Footer() {
   const date = new Date()
   const year = date.getFullYear()
   const name = process.env.REACT_APP_NAME;
+  const key = process.env.REACT_APP_API_KEY
   return (
     <div className='footer'>
-               <div>Copyright © {year} Breezy by {name} All Right Reserved. Powered by <a rel="noreferrer" target="_blank" href='https://openweathermap.org/'>OpenWeather</a></div>
+               <div>Copyright © {year} Breezy by {key} All Right Reserved. Powered by <a rel="noreferrer" target="_blank" href='https://openweathermap.org/'>OpenWeather</a></div>
     </div>
   )
 }
